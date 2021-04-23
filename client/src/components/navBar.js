15943e1f9ec5f6b2ff4673c440ca46fbb56f7b08
Mahery RAZAFINDRAJAO <razafindrajao.m@sfeir.com>
2021-04-23 19:59:49 +0200

    update navbar

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -5,23 +5,36 @@ import { Link } from 'react-router-dom';
 import './navBar.css';
 
 const logOut = (dispatch) => {
-  dispatch({ action: "loggedOut" });
-}
+  dispatch({ action: 'loggedOut' });
+};
 
-export default function NavBar(props){
-  const { state: {user}, action: {login, logout} , flash } = useStateValue();
-  
+export default function NavBar(props) {
+  const {
+    state: { user },
+    action: { login, logout },
+    flash,
+  } = useStateValue();
+
   return (
-    <div className="navbar_container">
-      <Link to="/"><button className="navbar_btn home">Home</button></Link>
-      <Link to="/users/index"><button className="navbar_btn users">Users</button></Link>
-      <Link to="/books/index"><button className="navbar_btn users">Books</button></Link>
-      {
-        user && 
-        ( <span className="navbar_user_part"> 
-            <span className="navbar_user_info">{ user.email }</span> <button className="navbar_btn" onClick={logout}> Logout </button>
-          </span>)
-      }
-    </div> 
+    <div className='navbar_container'>
+      <Link to='/' className='navbar_btn home'>
+        Home
+      </Link>
+      <Link to='/users/index' className='navbar_btn users'>
+        Users
+      </Link>
+      <Link to='/books/index' className='navbar_btn users'>
+        Books
+      </Link>
+      {user && (
+        <span className='navbar_user_part'>
+          <span className='navbar_user_info'>{user.email}</span>{' '}
+          <button className='navbar_btn' onClick={logout}>
+            {' '}
+            Logout{' '}
+          </button>
+        </span>
+      )}
+    </div>
   );
 }
